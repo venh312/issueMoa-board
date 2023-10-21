3dd853480b5ed4e3dae8ebb33c4c5307a4eb3eac
venh <conf312@naver.com>
2023-10-21 12:33:58 +0900

    [ADD] 국제화 메시지 추가

--- src/main/java/com/issuemoa/board/web/BoardController.java
+++ src/main/java/com/issuemoa/board/web/BoardController.java
@@ -4,21 +4,29 @@ import com.issuemoa.board.domain.Board;
 import com.issuemoa.board.message.RestMessage;
 import com.issuemoa.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Locale;
+
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class BoardController {
 
     private final BoardService boardService;
+    private final MessageSource messages;
 
     @GetMapping("/board/{type}/list")
-    public ResponseEntity<RestMessage> findAll(Board.Request request,
+    public ResponseEntity<RestMessage> findAll(Board.Request request, Locale locale,
                                   @RequestParam(required = false, defaultValue = "0") Integer page,
                                   @RequestParam(required = false, defaultValue = "24") Integer pageSize) {
+
+        log.info("local message : {}", messages.getMessage("board.select.empty", null, locale));
         return ResponseEntity.ok()
                 .headers(new HttpHeaders())
                 .body(new RestMessage(HttpStatus.OK, boardService.findAll(request, page, pageSize)));
