8b7c0e68d126b0c420588d6c408ee08f12491018
venh <conf312@naver.com>
2024-01-01 13:29:28 +0900

    [ADD] ApiResponses - Board 추가

--- src/main/java/com/issuemoa/board/domain/Board.java
+++ src/main/java/com/issuemoa/board/domain/Board.java
@@ -1,5 +1,6 @@
 package com.issuemoa.board.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,17 +9,31 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 import java.time.LocalDateTime;
 
+@Schema(name = "Board Response")
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Document(collection = "board")
 public class Board {
+    @Schema(description = "IDX")
     @MongoId
     private String id;
+
+    @Schema(description = "news / youtube")
     private String type;
+
+    @Schema(description = "제목")
     private String title;
+
+    @Schema(description = "내용")
     private String contents;
+
+    @Schema(description = "URL")
     private String url;
+
+    @Schema(description = "썸네일")
     private String thumbnail;
+
+    @Schema(description = "등록시간")
     private LocalDateTime registerDateTime;
 }
--- src/main/java/com/issuemoa/board/web/BoardController.java
+++ src/main/java/com/issuemoa/board/web/BoardController.java
@@ -1,9 +1,14 @@
 package com.issuemoa.board.web;
 
+import com.issuemoa.board.domain.Board;
 import com.issuemoa.board.message.RestMessage;
 import com.issuemoa.board.service.BoardService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +27,9 @@ public class BoardController {
     private final BoardService boardService;
     private final MessageSource messages;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(schema = @Schema(implementation = Board.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근")})
     @Operation(summary = "Issue 목록", description = "NEWS / YOUTUBE 목록을 불러온다.")
     @GetMapping("/board/{type}/list")
         public ResponseEntity<RestMessage> findAll(Locale locale,
